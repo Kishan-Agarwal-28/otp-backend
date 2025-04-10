5186242c732aecab080d37dc0ea16ce4461ce5ed
Kishan Agarwal <kishanagarwal028@gmail.com>
2025-04-10 21:02:03 +0530

    refactor: enhance CORS configuration in index.js for improved security and functionality

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(cors({origin:'*'}));
+app.use(cors({
+    origin:'*',
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type','Authorization','Accept','X-Requested-With','X-CSRF-Token'],
+}
+));
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}));
 app.use(cookieParser());
